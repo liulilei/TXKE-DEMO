7173abc6b0fa7bc03ed035eb4c1395c0d00ccbcd
liulei <CHX123OK>
2019-04-24 17:06:11 +0800

    翻页效果view和动画

--- app/src/main/java/androiddesk/com/txke_demo/TestViewActivity.kt
+++ app/src/main/java/androiddesk/com/txke_demo/TestViewActivity.kt
@@ -1,8 +1,8 @@
 package androiddesk.com.txke_demo
 
+import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Bundle
-import androiddesk.com.txke_demo.util.Uiutils
 import androidx.fragment.app.FragmentActivity
 import kotlinx.android.synthetic.main.activity_test_view.*
 
@@ -23,9 +23,26 @@ class TestViewActivity : FragmentActivity() {
 //                .rotation(90f)
 //                .setStartDelay(1000)
 //                .start()
-        val animator = ObjectAnimator.ofFloat(view, "radius", Uiutils.dip2px(150f))
-        animator.startDelay = 1000
-        animator.start()
+//        val animator = ObjectAnimator.ofFloat(view, "radius", Uiutils.dip2px(150f))
+//        animator.startDelay = 1000
+//        animator.start()
+
+        val topFlip = ObjectAnimator.ofFloat(view, "topFlip", -30f)
+        topFlip.duration = 1000
+        topFlip.startDelay = 200
+
+        val bottomFlip = ObjectAnimator.ofFloat(view, "bottomFlip", 30f)
+        bottomFlip.duration = 1000
+        bottomFlip.startDelay = 200
+
+        val flipRotation = ObjectAnimator.ofFloat(view, "flipRotation", 270f)
+        flipRotation.duration = 1500
+        flipRotation.startDelay = 200
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playSequentially(bottomFlip, flipRotation, topFlip)
+        animatorSet.startDelay = 500
+        animatorSet.start()
 
     }
 
---
+++ app/src/main/java/androiddesk/com/txke_demo/view/CameraAnimView.kt
@@ -0,0 +1,85 @@
+package androiddesk.com.txke_demo.view
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Camera
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import androiddesk.com.txke_demo.util.Uiutils
+
+/**
+ *@Description:
+ * @author: lll
+ * @date: 2019-04-24
+ */
+class CameraAnimView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null, defTypeAttr: Int = 0)
+    : View(context, attributeSet, defTypeAttr) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    private val WIDTH = Uiutils.dip2px(200f)
+
+    private val padding = Uiutils.dip2px(100f)
+
+    private val camera = Camera()
+
+    private var bitmap: Bitmap? = null
+
+    var topFlip = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var bottomFlip = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    var flipRotation = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    init {
+        bitmap = Uiutils.getAvatar(resources, WIDTH.toInt())
+        camera.setLocation(0f, 0f, Uiutils.getZLocation())
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        
+        canvas?.apply {
+            save()
+            translate(padding + WIDTH / 2, padding + WIDTH / 2)
+            rotate(-flipRotation)
+            camera.save()
+            camera.rotateX(topFlip)
+            camera.applyToCanvas(canvas)
+            camera.restore()
+            clipRect(-WIDTH, -WIDTH, WIDTH, 0f)
+            rotate(flipRotation)
+            translate(-(padding + WIDTH / 2), -(padding + WIDTH / 2))
+            drawBitmap(bitmap, padding, padding, paint)
+            restore()
+
+            save()
+            translate(padding + WIDTH / 2, padding + WIDTH / 2)
+            rotate(-flipRotation)
+            camera.save()
+            camera.rotateX(bottomFlip)
+            camera.applyToCanvas(canvas)
+            camera.restore()
+            clipRect(-WIDTH, 0f, WIDTH, WIDTH)
+            rotate(flipRotation)
+            translate(-(padding + WIDTH / 2), -(padding + WIDTH / 2))
+            drawBitmap(bitmap, padding, padding, paint)
+            restore()
+        }
+       
+    }
+}
