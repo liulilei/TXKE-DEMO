8251e32a537f80dc860a829032b00ab58765564b
liulilei <542910866@qq.com>
2019-06-27 09:11:59 +0800

    增加双指缩放

--- app/src/main/java/androiddesk/com/txke_demo/view/ScalableImageView.kt
+++ app/src/main/java/androiddesk/com/txke_demo/view/ScalableImageView.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.view.View
 import android.widget.OverScroller
 import androiddesk.com.txke_demo.util.UiUtils
@@ -23,8 +24,7 @@ class ScalableImageView @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null,
     defTypeAttr: Int = 0
-) : View(context, attributeSet, defTypeAttr), GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener,
-    Runnable {
+) : View(context, attributeSet, defTypeAttr) {
 
     private var paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
@@ -52,7 +52,15 @@ class ScalableImageView @JvmOverloads constructor(
 
     private var offerY = 0f
 
-    var scaleFraction = 0f
+    private var henGestureListener: HenGestureListener = HenGestureListener()
+
+    private var henRunnable: HenRunnable = HenRunnable()
+
+    private var scaleGestureDetector: ScaleGestureDetector? = null
+
+    private var henScaleListener: HenScaleListener = HenScaleListener()
+
+    var currentScale = 0f
         set(value) {
             field = value
             invalidate()
@@ -61,8 +69,9 @@ class ScalableImageView @JvmOverloads constructor(
     private var animator: ObjectAnimator? = null
 
     init {
-        gestureDetectorCompat = GestureDetectorCompat(context, this)
+        gestureDetectorCompat = GestureDetectorCompat(context, henGestureListener)
         overScroller = OverScroller(context)
+        scaleGestureDetector = ScaleGestureDetector(context, henScaleListener)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -77,86 +86,31 @@ class ScalableImageView @JvmOverloads constructor(
             bigScale = (width * 1.0f / bitMap.width) * scaleParam
             smallScale = height * 1.0f / bitMap.height
         }
+        currentScale = smallScale
     }
 
     private fun getAnimator(): ObjectAnimator? {
         if (animator == null) {
-            animator = ObjectAnimator.ofFloat(this, "scaleFraction", 0f, 1f)
+            animator = ObjectAnimator.ofFloat(this, "currentScale", 0f)
         }
+        animator?.setFloatValues(smallScale, bigScale)
         return animator
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        var scaleFraction = (currentScale - smallScale) / (bigScale - smallScale)
         canvas?.translate(offerX * scaleFraction, offerY * scaleFraction)
-        canvas?.scale(
-            smallScale + (bigScale - smallScale) * scaleFraction,
-            smallScale + (bigScale - smallScale) * scaleFraction,
-            (width / 2).toFloat(),
-            (height / 2).toFloat()
-        )
+        canvas?.scale(currentScale, currentScale, (width / 2).toFloat(), (height / 2).toFloat())
         canvas?.drawBitmap(bitMap, initOfferX, initOfferY, paint)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return gestureDetectorCompat?.onTouchEvent(event)!!
-    }
-
-    override fun onDoubleTap(e: MotionEvent?): Boolean {
-        isBig = !isBig
-        if (isBig) {
-            offerX = (e?.x!! - width / 2f) * (1f - bigScale / smallScale)
-            offerY = (e?.y!! - height / 2f) * (1f - bigScale / smallScale)
-            mathOffer()
-            getAnimator()?.start()
-        } else {
-            getAnimator()?.reverse()
+        var result = scaleGestureDetector?.onTouchEvent(event)
+        if (!scaleGestureDetector?.isInProgress!!) {
+            result = gestureDetectorCompat?.onTouchEvent(event)
         }
-        return false
-    }
-
-    override fun onDoubleTapEvent(e: MotionEvent?): Boolean {
-        return false
-    }
-
-    override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
-        return false
-    }
-
-    override fun onShowPress(e: MotionEvent?) {
-
-    }
-
-    override fun onSingleTapUp(e: MotionEvent?): Boolean {
-        return false
-    }
-
-    override fun onDown(e: MotionEvent?): Boolean {
-        return true
-    }
-
-    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
-        if (isBig) {
-            overScroller?.fling(
-                offerX.toInt(), offerY.toInt(), velocityX.toInt(), velocityY.toInt(),
-                (-((bitMap.width * bigScale) - width) / 2f).toInt(),
-                (((bitMap.width * bigScale) - width) / 2f).toInt(),
-                (-((bitMap.height * bigScale) - height) / 2f).toInt(),
-                (((bitMap.height * bigScale) - height) / 2f).toInt()
-            )
-            postOnAnimation(this)
-        }
-        return false
-    }
-
-    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
-        if (isBig) {
-            offerX -= distanceX
-            offerY -= distanceY
-            mathOffer()
-            invalidate()
-        }
-        return false
+        return result!!
     }
 
     private fun mathOffer() {
@@ -166,16 +120,98 @@ class ScalableImageView @JvmOverloads constructor(
         offerY = max(offerY, -((bitMap.height * bigScale) - height) / 2f)
     }
 
-    override fun onLongPress(e: MotionEvent?) {
+    private inner class HenGestureListener : GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+        override fun onDoubleTap(e: MotionEvent?): Boolean {
+            isBig = !isBig
+            if (isBig) {
+                offerX = (e?.x!! - width / 2f) * (1f - bigScale / smallScale)
+                offerY = (e?.y!! - height / 2f) * (1f - bigScale / smallScale)
+                mathOffer()
+                getAnimator()?.start()
+            } else {
+                getAnimator()?.reverse()
+            }
+            return false
+        }
 
+        override fun onDoubleTapEvent(e: MotionEvent?): Boolean {
+            return false
+        }
+
+        override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
+            return false
+        }
+
+        override fun onShowPress(e: MotionEvent?) {
+
+        }
+
+        override fun onSingleTapUp(e: MotionEvent?): Boolean {
+            return false
+        }
+
+        override fun onDown(e: MotionEvent?): Boolean {
+            return true
+        }
+
+        override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+            if (isBig) {
+                overScroller?.fling(
+                    offerX.toInt(), offerY.toInt(), velocityX.toInt(), velocityY.toInt(),
+                    (-((bitMap.width * bigScale) - width) / 2f).toInt(),
+                    (((bitMap.width * bigScale) - width) / 2f).toInt(),
+                    (-((bitMap.height * bigScale) - height) / 2f).toInt(),
+                    (((bitMap.height * bigScale) - height) / 2f).toInt()
+                )
+                postOnAnimation(henRunnable)
+            }
+            return false
+        }
+
+        override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+            if (isBig) {
+                offerX -= distanceX
+                offerY -= distanceY
+                mathOffer()
+                invalidate()
+            }
+            return false
+        }
+
+        override fun onLongPress(e: MotionEvent?) {
+
+        }
     }
 
-    override fun run() {
-        if (overScroller?.computeScrollOffset()!!) {
-            offerX = overScroller?.currX?.toFloat()!!
-            offerY = overScroller?.currY?.toFloat()!!
-            invalidate()
-            postOnAnimation(this)
+    private inner class HenRunnable : Runnable {
+        override fun run() {
+            if (overScroller?.computeScrollOffset()!!) {
+                offerX = overScroller?.currX?.toFloat()!!
+                offerY = overScroller?.currY?.toFloat()!!
+                invalidate()
+                postOnAnimation(this)
+            }
         }
+    }
+
+    private inner class HenScaleListener : ScaleGestureDetector.OnScaleGestureListener {
+
+        private var initScale = 0f
+
+        override fun onScaleBegin(detector: ScaleGestureDetector?): Boolean {
+            initScale = currentScale
+            return true
+        }
+
+        override fun onScaleEnd(detector: ScaleGestureDetector?) {
+
+        }
+
+        override fun onScale(detector: ScaleGestureDetector?): Boolean {
+            currentScale = initScale * detector?.scaleFactor!!
+            invalidate()
+            return false
+        }
+
     }
 }
