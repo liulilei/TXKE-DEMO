e9699a2f1b1f2bb0ab6edf6432a7011532d02044
liulilei <542910866@qq.com>
2019-07-03 15:06:56 +0800

    多只触控第一种协作是滑动

--- app/src/main/java/androiddesk/com/txke_demo/view/MultiTouchView.kt
+++ app/src/main/java/androiddesk/com/txke_demo/view/MultiTouchView.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import androiddesk.com.txke_demo.util.UiUtils
+
 
 /**
  *@Description:
@@ -18,18 +20,69 @@ class MultiTouchView @JvmOverloads constructor(
     defTypeAttr: Int = 0
 ) : View(context, attributeSet, defTypeAttr) {
 
+    private var startX = 0f
+
+    private var startY = 0f
+
+    private var offsetX = 0f
+
+    private var offsetY = 0f
+
+    private var endOffsetX = 0f
+
+    private var endOffestY = 0f
+
     private var paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     private val IMAGE_WIDTH = UiUtils.dip2px(200f)
 
     private var bitmap = UiUtils.getAvatar(resources, IMAGE_WIDTH.toInt())
 
-    init {
-
-    }
+    private var indexPointerId = 0
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawBitmap(bitmap, 0f, 0f, paint)
+        canvas?.drawBitmap(bitmap, offsetX, offsetY, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.actionMasked) {
+            MotionEvent.ACTION_DOWN -> {
+                indexPointerId = event.getPointerId(0)
+                startX = event.getX(0)
+                startY = event.getY(0)
+                endOffsetX = offsetX
+                endOffestY = offsetY
+            }
+            MotionEvent.ACTION_MOVE -> {
+                var currentIndex = event.findPointerIndex(indexPointerId)
+                offsetX = event.getX(currentIndex) - startX + endOffsetX
+                offsetY = event.getY(currentIndex) - startY + endOffestY
+                invalidate()
+            }
+            MotionEvent.ACTION_POINTER_DOWN -> {
+                indexPointerId = event.getPointerId(event.actionIndex)
+                startX = event.getX(event.actionIndex)
+                startY = event.getY(event.actionIndex)
+                endOffsetX = offsetX
+                endOffestY = offsetY
+            }
+            MotionEvent.ACTION_POINTER_UP -> {
+                val actionIndex = event.actionIndex
+                if (event.getPointerId(actionIndex) == indexPointerId) {
+                    var index = if (event.pointerCount - 1 == actionIndex) {
+                        event.pointerCount - 2
+                    } else {
+                        event.pointerCount - 1
+                    }
+                    indexPointerId = event.getPointerId(index)
+                    startX = event.getX(index)
+                    startY = event.getY(index)
+                    endOffsetX = offsetX
+                    endOffestY = offsetY
+                }
+            }
+        }
+        return true
     }
 }
